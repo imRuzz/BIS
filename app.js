356967c0bd7f846588eb984b2b55e94a31263071
Russel Dave Espineli <iamrusselespineli@gmail.com>
2024-08-06 22:11:16 +0800

    init

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 const view = path.join(__dirname, './view');
@@ -21,6 +23,24 @@ app.set('views', view);
 
 //Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 //Routes
 app.use('/', require('./routes/index'));
